81eec654c52e4852bf31433c556e6e686127408b
Stefan <stefankotarac01@gmail.com>
2022-05-23 09:47:33 +0200

    js

--- js2/script.js
+++ js2/script.js
@@ -1,15 +1,48 @@
-function addToCart(element){
-    let main = element.closest('.single-item');
-    let price = main.querySelector('.price').innerText;
-    let name = main.querySelector('h3').innerText;
-    let quantity = main.querySelector('input').value;
+let allTotal = 0;
+function addToCart(element) {
+    let mainEl = element.closest('.single-item');
+    let price = mainEl.querySelector('.price').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let quantity = mainEl.querySelector('input').value;
     let cartItems = document.querySelector('.cart-items');
- 
+
     if (parseInt(quantity) > 0) {
-        cartItems.innerHTML += `Added ${name} to cart`; 
+        price = price.substring(1);
+        price = parseInt(price);
+        let total = price * parseInt(quantity);
+        console.log(total);
+        allTotal += total;
+        cartItems.innerHTML += `<div class='cart-single-item'>
+                                    <h3>${name}</h3>
+                                    <p>$${price} x ${quantity} = $<span>${total}</span></p>
+                                    <button class = "remove item" onclick="removeFromCart(this)">Ukloni</button>
+        </div>`;
+        document.querySelector('.total').innerText = `Total: $${allTotal}`;
+
         element.innerText = 'X';
-        element.setAttribute('disabled', 'disabled');
+        element.setAttribute('disabled', 'true');
     } else {
         alert('Add your items to cart');
     }
-}  
+}
+function removeFromCart(element) {
+    let mainEl = element.closest('.cart-single-item');
+    let price = mainEl.querySelector('p span').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let vegetables = document.querySelectorAll('.single-item');
+    price = parseInt(price);
+
+    allTotal -= price;
+    document.querySelector('.total').innerText = `Total: $${allTotal}`;
+
+    mainEl.remove();
+
+    vegetables.forEach(function (vege) {
+        let itemName = vege.querySelector('.si-content h3').innerText;
+        if (itemName === name) {
+            vege.querySelector('.actions input').value = 0;
+            vege.querySelector('.actions button').removeAttribute('disabled');
+            vege.querySelector('.actions button').innerText = 'Add';
+        }
+    });
+}
